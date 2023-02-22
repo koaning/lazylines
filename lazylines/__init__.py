2cd0bcf22d5e1a52972527697bac6b78cd01fb4b
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-02-22 16:36:28 +0000

    clean

--- lazylines/__init__.py
+++ lazylines/__init__.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
-from typing import List, Union, Tuple
-from pathlib import Path
+
 import itertools as it
 import pprint
+from pathlib import Path
+from typing import Tuple, Union
 
 import srsly
 import tqdm
@@ -21,6 +22,7 @@ class LazyLines:
     from lazylines import LazyLines
     ```
     """
+
     def __init__(self, g):
         self.g = g
         self.groups = set()
@@ -33,25 +35,24 @@ class LazyLines:
         the speedup that comes at the cost of memory.
 
         ```python
-        from lazylines import LazyLines 
+        from lazylines import LazyLines
 
         items = ({"a": i} for i in range(100))
 
-        # The intermediate representation is now a list. 
+        # The intermediate representation is now a list.
         cached = (LazyLines(items).cache())
         ```
         """
         return LazyLines(g=list(self.g))
-
 
     def mutate(self, **kwargs) -> LazyLines:
         """
         Adds/overwrites keys in the dictionary based on lambda.
 
         **Usage**:
-        
+
         ```python
-        from lazylines import LazyLines 
+        from lazylines import LazyLines
 
         items = [{"a": 2}, {"a": 3}]
         results = (LazyLines(items).mutate(b=lambda d: d["a"] * 2))
@@ -75,7 +76,7 @@ class LazyLines:
         **Usage**:
 
         ```python
-        from lazylines import LazyLines 
+        from lazylines import LazyLines
 
         items = [{"a": 2}, {"a": 3}]
         results = (LazyLines(items).keep(lambda d: d["a"] % 2 == 0))
@@ -94,20 +95,20 @@ class LazyLines:
 
     def unnest(self, key) -> LazyLines:
         """
-        Explodes a key, effectively un-nesting it. 
+        Explodes a key, effectively un-nesting it.
 
         **Usage**:
 
         ```python
         from lazylines import LazyLines
 
-        data = [{'annotator': 'a', 
+        data = [{'annotator': 'a',
                  'subset': [{'accept': True, 'text': 'foo'},
                             {'accept': True, 'text': 'foobar'}]},
                 {'annotator': 'b',
                  'subset': [{'accept': True, 'text': 'foo'},
                             {'accept': True, 'text': 'foobar'}]}]
-        
+
         expected = [
             {'accept': True, 'annotator': 'a', 'text': 'foo'},
             {'accept': True, 'annotator': 'a', 'text': 'foobar'},
@@ -119,6 +120,7 @@ class LazyLines:
         assert result.collect() == expected
         ```
         """
+
         def new_gen():
             for item in self.g:
                 for value in item[key]:
@@ -165,20 +167,20 @@ class LazyLines:
 
     def __iter__(self):
         return self.g
-    
+
     def sort_by(self, *cols) -> LazyLines:
         """Sort the items."""
         return LazyLines(g=sorted(self.g, key=lambda d: tuple([d[c] for c in cols])))
-    
+
     def rename(self, **kwargs) -> LazyLines:
         """Rename a few keys in each item."""
+
         def new_gen():
             for item in self.g:
                 new_items = {k: item[v] for k, v in kwargs.items()}
                 yield {**item, **new_items}
-        
+
         return LazyLines(g=new_gen())
-        
 
     def nest_by(self, *args) -> LazyLines:
         """
@@ -199,14 +201,14 @@ class LazyLines:
         ]
 
         expected = [
-                {'annotator': 'a', 
+                {'annotator': 'a',
                  'subset': [{'accept': True, 'text': 'foo'},
                             {'accept': True, 'text': 'foobar'}]},
                 {'annotator': 'b',
                  'subset': [{'accept': True, 'text': 'foo'},
                             {'accept': True, 'text': 'foobar'}]}
         ]
-        
+
         result = LazyLines(data).nest_by("annotator")
         assert result.collect() == expected
         ```
@@ -234,9 +236,11 @@ class LazyLines:
         """Turns the collection into a list."""
         return [ex for ex in self.g]
 
-    def write_jsonl(self, path) -> LazyLines:
+    def write_jsonl(
+        self, path, append: bool = False, append_new_line: bool = True
+    ) -> LazyLines:
         """Write everything into a jsonl file again."""
-        srsly.write_jsonl(path, self.g)
+        srsly.write_jsonl(path, self.g, append=append, append_new_line=append_new_line)
 
     def select(self, *args) -> LazyLines:
         """Only select specific keys from each dictionary."""
@@ -252,7 +256,7 @@ class LazyLines:
 
         def new_gen():
             for ex in self.g:
-                yield {k: v for k, v in ex.items() if v not in args}
+                yield {k: v for k, v in ex.items() if k not in args}
 
         return LazyLines(g=new_gen())
 
@@ -262,8 +266,10 @@ class LazyLines:
 
     def foreach(self, func, *args, **kwargs) -> LazyLines:
         """Just call a function on each dictionary, but pass the original forward."""
+
         def new_gen():
             for ex in self.g:
                 func(ex, *args, **kwargs)
                 yield ex
+
         return LazyLines(g=new_gen())
--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,12 +1,13 @@
-from lazylines import LazyLines
-
 import pytest
 from mktestdocs import check_docstring, get_codeblock_members
 
-import inspect
-import textwrap
+from lazylines import LazyLines
+
 
 # Note the use of `__qualname__`, makes for pretty output
-@pytest.mark.parametrize("obj", get_codeblock_members(LazyLines), ids=lambda d: d.__qualname__)
+@pytest.mark.parametrize(
+    "obj", get_codeblock_members(LazyLines), ids=lambda d: d.__qualname__
+)
 def test_member(obj):
+    """Checks the docstrings as a unit test."""
     check_docstring(obj)
