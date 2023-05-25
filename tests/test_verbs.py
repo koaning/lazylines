27f7e545e4893731ecfdbde1d66f52848f32554f
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-05-25 19:17:04 +0000

    more tests

---
+++ tests/test_verbs.py
@@ -0,0 +1,30 @@
+from lazylines import LazyLines
+import pytest 
+
+@pytest.fixture
+def data():
+    return ({"a": i, "b": i+1, "c": i + 2, "d": i + 3} for i in range(100))
+
+
+def test_rename(data):
+    items = LazyLines(data).rename(c="b").collect()
+    for item in items:
+        assert item["a"] + 1 == item["c"]
+
+
+def test_select(data):
+    items = LazyLines(data).select("a", "b").collect()
+    for item in items:
+        assert "a" in item
+        assert "b" in item
+        assert "c" not in item
+        assert "d" not in item
+
+
+def test_drop(data):
+    items = LazyLines(data).drop("a", "b").collect()
+    for item in items:
+        assert "a" not in item
+        assert "b" not in item
+        assert "c" in item
+        assert "d" in item
