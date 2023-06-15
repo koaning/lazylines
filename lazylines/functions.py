7adfe78b55f4cd13384c881cc882e76ae594d2fe
vincent d warmerdam <vincentwarmerdam@gmail.com>
2023-06-15 14:55:38 +0200

    Update functions.py

--- lazylines/functions.py
+++ lazylines/functions.py
@@ -9,3 +9,9 @@ def round_timestamp(ts, to="day"):
     if to not in mapping:
         raise ValueError(f"`to` must be in {mapping.keys}. got {to}.")
     return str(dt.datetime.fromtimestamp(ts).strftime(mapping[to]))
+
+
+def to_nested_pairs(lines, subset_key="subset"):
+    for ex in lines:
+        for c1, c2 in it.combinations(ex[subset_key], r=2):
+            yield {**ex, "subset": [c1, c2]}
