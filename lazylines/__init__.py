b3d6ad57e6f66423feecc029d04659ae25620ae0
koaning <vincentwarmerdam@gmail.com>
2025-11-10 12:26:04 +0100

    explode verb added

--- lazylines/__init__.py
+++ lazylines/__init__.py
@@ -104,7 +104,7 @@ class LazyLines:
 
     def unnest(self, key: str="subset") -> LazyLines:
         """
-        Explodes a key, effectively un-nesting it.
+        Un-nests a list of dictionaries in a key
 
         Arguments:
             key: the key to un-nest
@@ -129,6 +129,42 @@ class LazyLines:
         ]
 
         result = LazyLines(data).unnest("subset").collect()
+        assert result == expected
+        ```
+        """
+
+        def new_gen():
+            for item in self.g:
+                for value in item[key]:
+                    orig = {k: v for k, v in item.items() if k != key}
+                    d = {**value, **orig}
+                    yield d
+
+        return LazyLines(g=new_gen())
+
+    def explode(self, key: str) -> LazyLines:
+        """
+        Explodes a key, effectively un-nesting it.
+
+        Arguments:
+            key: the key to explode
+
+        **Usage**:
+
+        ```python
+        from lazylines import LazyLines
+
+        data = [{'foo': 'a',
+                 'bar': [1, 2, 3],
+                }]
+
+        expected = [
+            {'foo': 'a', 'bar': 1},
+            {'foo': 'a', 'bar': 2},
+            {'foo': 'a', 'bar': 3},
+        ]
+
+        result = LazyLines(data).explode("bar").collect()
         assert result == expected
         ```
         """
